7d380ad04e42db4173501020e2ea497d0a748b4b
sanskar <sanskargupta15@gmail.com>
2023-03-23 00:02:21 +0530

    asd

--- app/main.py
+++ app/main.py
@@ -12,8 +12,8 @@ def main():
     args = sys.argv[4:]
     #
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
-
+    sys.stdout.write(completed_process.stdout.decode("utf-8"))
+    sys.stderr.write(completed_process.stderr.decode("utf-8"))
 
 if __name__ == "__main__":
     main()
